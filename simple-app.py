e569afe3939a7c662e33fdfde4ae647653eb19a8
root691 <23039838+root691@users.noreply.github.com>
2023-09-23 01:51:10 +0300

    Update simple-app.py

--- simple-app.py
+++ simple-app.py
@@ -1,5 +1,19 @@
 import sys, os
 
-print("Files in current directory:")
-for file in os.listdir("./"):
-  print(file)
+args = sys.argv[1:]
+
+if len(args) > 0:
+    current_dir = os.getcwd()
+    for file in args:
+        full_file_path = os.path.join(current_dir, file)
+        if os.path.isfile(full_file_path):
+            print(f"Printing {full_file_path} file content:")
+            with open(full_file_path, 'r') as f:
+                print(f.read())
+                f.close()
+            print()
+            print()
+else:
+    print("Files in current directory:")
+    for file in os.listdir("./"):
+      print(file)
